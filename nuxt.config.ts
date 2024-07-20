245edf8587496ccf151674044fa4bbc41866b7e8
mirsella <mirsella@protonmail.com>
2024-07-20 20:40:10 +0200

    disable nuxt's ssr for capacitor

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
-})
+  modules: ["@nuxtjs/tailwindcss"],
+  // for capacitor
+  ssr: false,
+});
