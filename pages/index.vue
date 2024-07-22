134f93d140a872a07f5964ac41f0d8939a6288f1
mirsella <mirsella@protonmail.com>
2024-07-22 18:57:39 +0200

    start homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
-const { $signout } = useNuxtApp();
+const { $signout, $getLoginData } = useNuxtApp();
+const login = ref("lgillard");
+const data = ref({});
+async function search() {
+  try {
+    data.value = await $getLoginData(login.value);
+  } catch (error) {
+    console.error(error);
+    showError("erreur lors de la recherche: " + error);
+  }
+}
 </script>
 
 <template>
-  <div class="flex items-center justify-center gap-2">
-    <button class="btn btn-primary" @click="$signout()">signout</button>
+  <div class="mx-auto max-w-xl space-y-8">
+    <button class="btn btn-primary btn-wide" @click="$signout()">
+      signout
+    </button>
+    <div class="space-x-4">
+      <input
+        v-model="login"
+        type="text"
+        class="input input-primary"
+        placeholder="login"
+      />
+      <button class="btn btn-primary" @click="search()">search</button>
+    </div>
+    {{ data }}
   </div>
 </template>
