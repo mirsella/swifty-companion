a213263bf81b5e18a29c20dd9f2c551ada7bec6d
mirsella <mirsella@protonmail.com>
2024-08-21 17:03:55 +0200

    improve error handling, move some function, remove no-cors from 42 api fetch

--- plugins/42.client.ts
+++ plugins/42.client.ts
@@ -21,7 +21,7 @@ export default defineNuxtPlugin(async (nuxtApp) => {
 
   // grant_type can be "authorization_code" or "refresh_token"
   async function getToken(code: string, grant_type: string) {
-    console.log("getToken", grant_type);
+    console.log("getToken grant_type", grant_type);
     const formData = new FormData();
     formData.append("grant_type", grant_type);
     formData.append("code", code);
@@ -29,12 +29,15 @@ export default defineNuxtPlugin(async (nuxtApp) => {
     formData.append("client_secret", runtimeConfig.public.CLIENT_SECRET);
     formData.append("redirect_uri", oauth2Options.web.redirectUrl);
     const response = await fetch("https://api.intra.42.fr/oauth/token", {
-      mode: "no-cors",
+      // mode: "no-cors",
       method: "POST",
       body: formData,
     });
+    if (!response.ok) {
+      throw new Error("getToken response not ok: " + response);
+    }
     const data = await response.json();
-    console.log("getToken", data);
+    console.log("getToken data", data);
     localStorage.token = data.access_token;
     localStorage.tokenExpiry = data.secret_valid_until;
     localStorage.refreshToken = data.refresh_token;
@@ -48,39 +51,15 @@ export default defineNuxtPlugin(async (nuxtApp) => {
     }
   }
 
-  try {
-    let token = localStorage.token;
-    if (!token) {
-      console.log("No token, starting oauth");
-      await GenericOAuth2.authenticate(oauth2Options)
-        .then(async (res) => {
-          const code = res.authorization_response.code;
-          if (!code) {
-            throw new Error(
-              "Authorization process did not return a code: " +
-                JSON.stringify(res),
-            );
-          }
-          console.log("Got auth code", code);
-          await getToken(code, "authorization_code");
-        })
-        .catch((e) => showError(e));
-    }
-    await refreshTokenIfNeeded();
-  } catch (error) {
-    console.error("Authentication error:", error);
-  }
-  console.log("42 plugin loaded and authenticated");
-
   async function getLoginData(login: string): Promise<object> {
     refreshTokenIfNeeded();
     const res = await fetch(`https://api.intra.42.fr/v2/users/${login}`, {
-      mode: "no-cors",
+      // mode: "no-cors",
       headers: {
         Authorization: `Bearer ${localStorage.token}`,
       },
     }).then((e) => e.json());
-    console.log(res);
+    console.log("getLoginData", res);
     return res;
   }
 
@@ -88,6 +67,34 @@ export default defineNuxtPlugin(async (nuxtApp) => {
     localStorage.removeItem("token");
     localStorage.removeItem("tokenExpiry");
     reloadNuxtApp();
+  }
+
+  // auto login
+  try {
+    if (!localStorage.token) {
+      console.log("No token, starting oauth");
+      await GenericOAuth2.authenticate(oauth2Options).then(async (res) => {
+        const code = res.authorization_response.code;
+        if (!code) {
+          throw new Error(
+            "Authorization process did not return a code: " +
+              JSON.stringify(res),
+          );
+        }
+        console.log("Got auth code", code);
+        await getToken(code, "authorization_code");
+      });
+    }
+    await refreshTokenIfNeeded();
+  } catch (error: any) {
+    console.error("Authentication error:", error);
+    showError("Authentication error: " + error);
+  }
+
+  if (localStorage.token) {
+    console.log("42 plugin loaded and authenticated");
+  } else {
+    showError("42 plugin loaded but not authenticated");
   }
 
   return {
