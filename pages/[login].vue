ee49debd7be221b23cfc609d9f037d32dd9be370
mirsella <mirsella@protonmail.com>
2024-08-25 18:49:15 +0200

    improve loading indicator, error handling

--- pages/[login].vue
+++ pages/[login].vue
@@ -2,16 +2,20 @@
 const { $getLoginData, $signout } = useNuxtApp();
 const route = useRoute();
 const data = ref<UserApiResponse | null>(null);
-try {
-  const res = await $getLoginData(route.params.login as string);
-  if (!res) {
-    await navigateTo("/?error=login not found");
+
+(async () => {
+  try {
+    const res = await $getLoginData(route.params.login as string);
+    if (!res) {
+      await navigateTo("/?error=login not found");
+    }
+    data.value = res;
+  } catch (error) {
+    console.error(error);
+    await $signout();
   }
-  data.value = res;
-} catch (error) {
-  console.error(error);
-  await $signout();
-}
+})();
+
 const skills = computed(() => {
   if (!data.value) {
     return [];
@@ -28,53 +32,60 @@ const skills = computed(() => {
 
 <template>
   <div>
-    <div class="card card-compact bg-base-100 shadow-xl m-4">
-      <figure>
-        <img class="rounded-lg" :src="data?.image.link" alt="avatar" />
-      </figure>
-      <div class="card-body">
-        <span class="card-title"
-          >{{ data?.displayname }} ({{ data?.login }})</span
-        >
-        <div><span class="i-carbon-email mx-2"></span>{{ data?.email }}</div>
-        <div><span class="i-carbon-phone mx-2"></span>{{ data?.phone }}</div>
-        <div><span class="i-carbon-wallet mx-2"></span>{{ data?.wallet }}₳</div>
-        <div>
-          <span class="i-carbon-chart-point mx-2"></span
-          >{{ data?.correction_point }} points
-        </div>
-      </div>
+    <div v-if="!data" class="flex justify-center h-36">
+      <span class="!w-16 loading loading-infinity"></span>
     </div>
-    <div class="card card-compact bg-base-100 shadow-xl m-4">
-      <div class="card-body">
-        <span class="card-title">Skills</span>
-        <div v-for="skill in skills" :key="skill.id" class="m-2">
-          <span class="font-bold"> {{ skill.name }}: </span>
-          {{ skill.level.toFixed(2) }}
-        </div>
-      </div>
-    </div>
-    <div class="card card-compact bg-base-100 shadow-xl m-4">
-      <div class="card-body">
-        <span class="card-title">Projects</span>
-        <div
-          v-for="project in data?.projects_users"
-          :key="project.id"
-          class="m-2 shadow-md rounded-lg p-1"
-          :class="{
-            'shadow-error': project['validated?'] === false,
-            'shadow-success': project['validated?'] === true,
-          }"
-        >
-          <div class="w-full">
-            <span class="font-bold">{{ project.project.name }}</span> status:
-            {{ project.status }}
+    <template v-else>
+      <div class="card card-compact bg-base-100 shadow-xl m-4">
+        <figure>
+          <img class="rounded-lg" :src="data?.image.link" alt="avatar" />
+        </figure>
+        <div class="card-body">
+          <span class="card-title"
+            >{{ data?.displayname }} ({{ data?.login }})</span
+          >
+          <div><span class="i-carbon-email mx-2"></span>{{ data?.email }}</div>
+          <div><span class="i-carbon-phone mx-2"></span>{{ data?.phone }}</div>
+          <div>
+            <span class="i-carbon-wallet mx-2"></span>{{ data?.wallet }}₳
           </div>
-          <span v-if="project.final_mark">
-            mark: {{ project.final_mark }}
-          </span>
+          <div>
+            <span class="i-carbon-chart-point mx-2"></span
+            >{{ data?.correction_point }} points
+          </div>
         </div>
       </div>
-    </div>
+      <div class="card card-compact bg-base-100 shadow-xl m-4">
+        <div class="card-body">
+          <span class="card-title">Skills</span>
+          <div v-for="skill in skills" :key="skill.id" class="m-2">
+            <span class="font-bold"> {{ skill.name }}: </span>
+            {{ skill.level.toFixed(2) }}
+          </div>
+        </div>
+      </div>
+      <div class="card card-compact bg-base-100 shadow-xl m-4">
+        <div class="card-body">
+          <span class="card-title">Projects</span>
+          <div
+            v-for="project in data?.projects_users"
+            :key="project.id"
+            class="m-2 shadow-md rounded-lg p-1"
+            :class="{
+              'shadow-error': project['validated?'] === false,
+              'shadow-success': project['validated?'] === true,
+            }"
+          >
+            <div class="w-full">
+              <span class="font-bold">{{ project.project.name }}</span> status:
+              {{ project.status }}
+            </div>
+            <span v-if="project.final_mark">
+              mark: {{ project.final_mark }}
+            </span>
+          </div>
+        </div>
+      </div>
+    </template>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 const login = ref("");
-const loading = ref(false);
 async function search() {
-  loading.value = true;
   await navigateTo(login.value || "lgillard");
 }
 </script>
@@ -18,9 +16,6 @@ async function search() {
       placeholder="login (default: lgillard)"
       @keyup.enter="search()"
     />
-    <button class="btn btn-primary w-full" @click="search()">
-      <span v-if="loading" class="loading loading-lg loading-infinity"></span>
-      search
-    </button>
+    <button class="btn btn-primary w-full" @click="search()">search</button>
   </div>
 </template>
